58f24ec39ffe6541fc861ca6bbdc87499f57c39c
Kyle Grenier <kyle.grenier11@gmail.com>
2023-06-09 15:15:10 -0700

    Trackables for radar system
    Radar pulses every X seconds, displaying trackables on the radar

---
+++ Assets/Scripts/Trackable.cs
@@ -0,0 +1,46 @@
+﻿
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Trackable : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+
+    [SerializeField] private TrackableData data;
+    [SerializeField] private Channel triggerPulseChannel;
+
+    #region Event Subbing
+    private void OnEnable()
+    {
+        triggerPulseChannel.OnEventRaised += Pulse;
+    }
+
+    private void OnDisable()
+    {
+        triggerPulseChannel.OnEventRaised -= Pulse;
+    }
+    #endregion
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        SetAlpha(0f);
+    }
+
+    private void Pulse()
+    {
+        LeanTween.value(gameObject, SetAlpha, 1f, 0f, data.displayTime).setEase(data.easingType);
+    }
+
+    private void SetAlpha(float alpha)
+    {
+        Color c = spriteRenderer.color;
+        c.a = alpha;
+
+        spriteRenderer.color = c;
+    }
+}
